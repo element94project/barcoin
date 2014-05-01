f1fcb491eb35bfde066120b33bada55d7b8aabfd
element94project <element94@gmail.com>
2014-05-01 03:29:29 -0500

    epochtime

--- src/main.cpp
+++ src/main.cpp
@@ -2014,7 +2014,7 @@ bool LoadBlockIndex(bool fAllowNew)
         block.hashPrevBlock = 0;
         block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1398923738; //epochtime
+        block.nTime    = 1398932884; //epochtime
         block.nBits    = 0x1e0ffff0;
         block.nNonce   = 0;
 
