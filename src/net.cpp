0b1014cf260c698181f79a6d05bed29236a2bc89
element94project <element94@gmail.com>
2014-05-01 01:36:03 -0500

    build

--- src/net.cpp
+++ src/net.cpp
@@ -49,8 +49,7 @@ static bool vfReachable[NET_MAX] = {};
 static bool vfLimited[NET_MAX] = {};
 static CNode* pnodeLocalHost = NULL;
 uint64 nLocalHostNonce = 0;
-array<int, THREAD_MAX> vnThreadsRunning;
-static std::vector<SOCKET> vhListenSocket;
+boost::array<int, THREAD_MAX> vnThreadsRunning;static std::vector<SOCKET> vhListenSocket;
 CAddrMan addrman;
 
 vector<CNode*> vNodes;
